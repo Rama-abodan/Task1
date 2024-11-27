9055f8cfe790fb47d60b4cdf40ffa63c822cf8e8
Rama-abodan <ramaabodan06@gmail.com>
2024-11-27 15:07:06 +0300

    up 2

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layout.app')
 @section('title', 'show post')
 @section('content')
-    
+
         <h1>title:{{ $post->title }}</h1>
         <h1>description:{{ $post->description }}</h1>
         <img src="/images/posts/{{ $post->image }}" alt="">
         <p>add at: {{ $post->created_at }}</p>
         <a href="{{ route('posts.index') }}" class="btn btn-secondary">Back to home page</a>
-    
+        
 
 
 @endsection
